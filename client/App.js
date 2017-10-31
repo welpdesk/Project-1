13fbf6ab17315b4d31a6e05543807320250a1a72
Julie Moon <8juliemoon@gmail.com>
2017-10-31 11:38:35 -0400

    renders hello world

---
+++ client/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class 
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+    document.getElementById('root')
+);
---
+++ client/Homepage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const relJobs = () => {
+    return <h1>Relevant Jobs</h1>
+}
+
+const appliedTo = () =>{
+    
+}
+
+const listMaker = () =>{
+
+}
+
+ReactDOM.render(
+    <h1></h1>
+)
+
+// module.exports = FeedItem;
