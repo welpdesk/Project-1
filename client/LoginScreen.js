1e5292e57b37895b0fcf3e7729937c1eee38b981
Julie Moon <8juliemoon@gmail.com>
2017-11-01 18:37:30 -0400

    pass props

--- client/LoginScreen.js
+++ client/LoginScreen.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Login from './Login'
 
-const LoginScreen =()=>{
-    return (
-        <div class="loginWindow">
-            <Login/>
-        </div>
-    )
+class LoginScreen extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+        <div className="loginWindow">
+            <h1>Log in</h1>
+            <Login logIntoPage={this.props.logIntoPage} onPasswordChange={this.props.onPasswordChange} onUsernameChange={this.props.onUsernameChange}/>
+        </div> 
+        )
+    }
 }
 
 export default LoginScreen;
