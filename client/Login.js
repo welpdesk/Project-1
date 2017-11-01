d4e353a4d915c51c23a1fd52485bd8f7530d3082
Julie Moon <8juliemoon@gmail.com>
2017-11-01 18:37:00 -0400

    pass props for onChange

--- client/Login.js
+++ client/Login.js
@@ -3,29 +3,16 @@ import React, { Component } from 'react'
 class Login extends Component {
   constructor(props){
     super(props)
-    this.state = {value:'', password:''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange (event)  {
-    this.setState({value:event.target.value});
-  }
-
-  handlePasswordChange (event){
-    this.setState({password: event.target.value});
-  }
-  
-  handleSubmit (event){
-    event.preventDefault();
-  }
   render(){
-    <form onSubmit={this.props.onSubmit}>
-    <Input type='text' name='username' placeholder='username' />
-    <Input type='password' name='password' placeholder='password' />
-    <button> Sign In</button>
-    </form>  
+    return(
+      <div>
+      <input type='text' name='username' placeholder='username' onChange={this.props.onUsernameChange}></input>
+      <input type='password' name='password' placeholder='password' onChange={this.props.onPasswordChange} ></input>
+      <button onClick={this.props.logIntoPage}> Sign In</button>
+      </div>
+    )
   }
 }
 export default Login
