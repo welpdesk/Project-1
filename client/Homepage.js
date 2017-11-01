63c671ab6d9ff601488a7ee4fb632449461b0f27
Julie Moon <8juliemoon@gmail.com>
2017-11-01 13:41:15 -0400

    add search bar to homepage

--- client/Homepage.js
+++ client/Homepage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import List from './List'
+import Searchbar from './Searchbar'
 
 const Homepage = () => {
     return (
         <div className="Homepage">
+            <Searchbar />
             <List title ={'Relevant Jobs'}/>
             <List title = {'You have Applied To These Jobs:'}/>
         </div>
---
+++ client/Searchbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SearchBar= ()=>{
+    
+        return( 
+        <input type="text" class="searchTerm" placeholder="What are you looking for?"></input>
+        )
+    
+}
+
+export default SearchBar;
