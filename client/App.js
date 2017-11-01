0962ccf3f57960019289623c29bb1a348678bd11
Shachy Rivas <shachyrivas@Shachys-MacBook-Air.local>
2017-11-01 12:21:04 -0400

    Adds routing for all pages

--- client/App.js
+++ client/App.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Homepage from './Homepage'
-import LoginScreen from './LoginScreen'
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Redirect, Route} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import Homepage from './Homepage';
+import LoginScreen from './LoginScreen';
+import PrivateRoute from './PrivateRoute.js';
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render () {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={ () => ( <LoginScreen /> ) } />
+          <Route path="/register" render={ () => {} } />
+          <PrivateRoute path="/home" component={Homepage} isAuth={true} />
+        </Switch>
+      </BrowserRouter>
+    )
+  };
+} 
 
 
 ReactDOM.render(
-    < Homepage />,
+    < App />,
     document.getElementById('root')
 );
